d483aae766fe242c8bda4024556f0a05a466ad3c
Dan <DanielMooreRealPerson@gmail.com>
2021-09-20 23:51:36 -0400

    commit tree da36868e1055e28186803f9a486f06601397f882

---
+++ build/precache-manifest.8b6ca522e392adac7e6fe0b2cac5e41a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9cc7b224ebc816ce943",
+    "url": "/static/css/main.0260c952.chunk.css"
+  },
+  {
+    "revision": "d9cc7b224ebc816ce943",
+    "url": "/static/js/main.f3c4ec25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "4534afac629ee499e44277a8ad86cc5f",
+    "url": "/index.html"
+  }
+];
